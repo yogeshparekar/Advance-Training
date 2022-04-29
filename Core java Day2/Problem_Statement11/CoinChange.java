8f51a57b7bb4a7f9a324f680f1383ee6ec69e1c2
Yogesh <yogeshparekar01@gmail.com>
2022-04-29 12:03:46 +0530

    Advance Training Day2 complete

---
+++ Core java Day2/Problem_Statement11/CoinChange.java
@@ -0,0 +1,47 @@
+package Problem_Statement11;
+
+import java.util.Scanner;
+import java.util.Vector;
+	  
+public class CoinChange {
+	  
+	   
+	    static int deno[] = {1, 2, 5, 10, 20,50, 100, 500, 2000};
+	    static int n = deno.length;
+	  
+	    static void findMin(int V)
+	    {
+	   	        Vector<Integer> ans = new Vector<>();
+	  
+	       	        for (int i = n - 1; i >= 0; i--)
+	        {
+	           
+	            while (V >= deno[i]) 
+	            {
+	                V -= deno[i];
+	                ans.add(deno[i]);
+	            }
+	        }
+	  
+	        
+	        for (int i = 0; i < ans.size(); i++)
+	        {
+	            System.out.print(" " + ans.elementAt(i));
+	                
+	            
+	        }
+	        System.out.println("\n The minimum number of coins is "+ans.size());
+	    }
+	  
+	   
+	    public static void main(String[] args) 
+	    {
+	    	 Scanner scanner=new Scanner(System.in);
+	    	   System.out.println("Enter the Number");
+	    	   int n=scanner.nextInt();
+	        
+	    	   
+	        System.out.print(" Following is minimal number " +"of change for " + n + ": ");
+	        findMin(n);
+	    }
+	}
